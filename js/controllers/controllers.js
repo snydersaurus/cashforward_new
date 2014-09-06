50e245d11872f3adf99984b3315c3e66d441018b
snyderbill <snyderbill@bfusa.com>
2014-09-06 09:14:34 -0400

    Stubbing service for all payments

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -6,7 +6,10 @@ angular.module('cashforward.controllers', [])
   .controller('OverviewController', ['$scope','$routeParams',function($scope, $routeParams) {
 	
  }])
-  .controller('BillsDepositsController', ['$scope','$routeParams',function($scope, $routeParams) {
-	
- }])
+  .controller('BillsDepositsController', ['$scope','$routeParams','PaymentService',
+                                          function($scope, $routeParams, PaymentService) {
+	  PaymentService.getCurrentPayments().then(function(result){
+		  $scope.transactions = result.data
+	  })
+ }]);
   
--- js/services/services.js
+++ js/services/services.js
@@ -3,10 +3,13 @@
 angular.module('cashforward.services', ['ngResource'])
 .factory('PaymentService', ['$http','$q',
   function($http, $q){
-	
     return {
-    	
+    	getCurrentPayments: function(){
+    		return $http.get('test/data/payments.json').success(function(result) {
+                return result.data
+            });
+    	}
     }
-}]);
+}])
 
 
